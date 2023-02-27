aa171175f55e204d23e1c4e17c2ef6e656306817
Karunakaran <121922921+karuna2110@users.noreply.github.com>
2023-02-27 21:07:17 +0530

    Add files via upload

---
+++ json resume/script.js
@@ -0,0 +1,52 @@
+let myResume={
+    "basics": {
+      "name": "karunakaran.T",
+      "email": "karunakaran2197@gmail.com",
+      "phone": 7639415610,
+      "degree": "B.E",
+      "location": {
+        "address": "6/18,indra nagar,periyammapalayam,veepanthattai",
+        "postalCode":621110,
+        "city": "perambalur",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          
+          "github":"https://github.com/karuna2110"
+        }
+      ]
+    },
+    
+      
+    
+    "education": [
+      {
+        "institution": "panimalar Engineering college",
+        "department": "Electrical and Electronics Engineering",
+        "studyType": "fulltime",
+        "batch start year": 2016,
+        "batch end year": 2019,
+        "gpa": 5.9,
+      }
+    ],
+    "skills": [
+      {
+        "name": "javascript",
+        "level": "beginer",
+     
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "script writter",
+      }
+    ]
+  }
+  console.log(myResume);
